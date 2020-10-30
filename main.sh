0cf74397d04d12d80be54a6b721ba8f80f5652de
Subhajit Sahu <wolfram77@gmail.com>
2020-10-30 12:26:55 +0530

    removed empty directories

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
-#$1 is the input filename
-#$2 is the number of nodes in the input file.
-#$3 is the outputfile.
-#$4 is the number of threads for computing mcb.
+# $1: input file
+# $2: number of nodes
+# $3: output file
+# $4: threads
 
 ulimit -s unlimited
 
@@ -11,22 +11,15 @@ mkdir -p Results
 rm bicc_output/* -f
 
 echo "Invoking bicc_decomposition..."
-
 bicc/bicc_decomposition $1 bicc_output/ 0 $2 0 1 >> /dev/null
-
 statsFile="bicc_output/stats"
 
 while read line;
 do
 	#next steps
  	#echo $firstline ; file which is used to call the maxclique pmc
- 	
  	filename=$(echo $line|cut -f 1 -d " ")".mtx"
-
  	bicc/Relabeller "bicc_output/"$filename $filename 1
-
  	sh run.sh $filename "Results/"$3 $4
-
  	rm -rf $filename
-
 done < $statsFile
--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
-#Ist argument implies the Input filename.
-#2nd argument implies the output filename.
-#3rd argument implies the number of threads.
-
+# $1: input file
+# $2: output file
+# $3: threads
 mcb/mcb_cuda $1 $2"_R.txt" $3
 mcb/mcb_cpu $1 $2"_C.txt" $3
 mcb/mcb_cpu_baseline $1 $2"_W.txt" $3
