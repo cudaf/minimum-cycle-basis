414f0fc7487430582f45e35508609e7ee7ed8f66
Subhajit Sahu <wolfram77@gmail.com>
2020-11-17 15:01:24 +0530

    :bus:

--- include/cycle_searcher.h
+++ include/cycle_searcher.h
@@ -42,7 +42,7 @@ struct cycle_storage {
   }
 
   void add_cycle(unsigned root, unsigned u, unsigned v, cycle *cle) {
-    unsigned long long index = combine(std::min(u, v), std::max(u, v));
+    uint64_t index = combine(std::min(u, v), std::max(u, v));
     if (list_cycles[root].find(index) == list_cycles[root].end())
       list_cycles[root].insert(std::make_pair(index, new list_common_cycles(cle)));
     else
