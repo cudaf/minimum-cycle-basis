dd8062789f1ce6bade4e110b0e7a3cd463d99dd6
Subhajit Sahu <wolfram77@gmail.com>
2020-11-17 13:01:04 +0530

    :bus:

--- gpu/common.cuh
+++ gpu/common.cuh
@@ -1,6 +1,4 @@
-#ifndef __H_COMMON_CUDA
-#define __H_COMMON_CUDA
-
+#pragma once
 #include "gpu_struct.cuh"
 
 #define calculate_8bit(X) (X)
@@ -16,14 +14,8 @@ extern dim3 dimGrid;
 extern dim3 dimBlock;
 
 extern "C" void init_cuda();
-
 extern "C" size_t configure_grid(int start, int end);
-
 extern "C" unsigned* allocate_pinned_memory(int chunk, int nodes);
-
 extern "C" void free_pinned_memory(unsigned *);
-
 extern "C" size_t calculate_chunk_size(size_t num_nodes, size_t num_edges,
     size_t size_vector, size_t nstream);
-
-#endif
