b86b99b0fa05c8da266aaecb1880a8c5bf834ed6
Subhajit Sahu <wolfram77@gmail.com>
2020-11-17 11:17:08 +0530

    :bus:

--- include/cycles.h
+++ include/cycles.h
@@ -1,21 +1,17 @@
-#ifndef __CYCLES_H
-#define __CYCLES_H
-
+#pragma once
+#include <utility>
+#include <unordered_map>
+#include <set>
+#include <assert.h>
 #include "BitVector.h"
 #include "compressed_trees.h"
 
-#include <unordered_map>
-#include <assert.h>
-#include <set>
-#include <utility>
 
 struct cycle {
   compressed_trees *trees;
-
   int root;
   unsigned non_tree_edge_index;
   int total_length;
-
   int ID;
 
   bool operator<(const cycle &rhs) const {
@@ -41,49 +37,39 @@ struct cycle {
   // std::set<unsigned> *get_edges()
   // {
   //   std::set<unsigned> *edges = new std::set<unsigned>();
-
   //   csr_multi_graph *parent_graph = tree->parent_graph;
-
   //   unsigned row = parent_graph->rows->at(non_tree_edge_index);
   //   unsigned col = parent_graph->columns->at(non_tree_edge_index);
-
   //   while(row != tree->root)
   //   {
   //     unsigned edge_offset = tree->parent_edges->at(row);
   //     unsigned reverse_edge_offset = parent_graph->reverse_edge->at(edge_offset);
-
   //     edges->insert(std::min(edge_offset,reverse_edge_offset));
-
   //     row = parent_graph->rows->at(edge_offset);
   //   }
-
   //   while(col != tree->root)
   //   {
   //     unsigned edge_offset = tree->parent_edges->at(col);
   //     unsigned reverse_edge_offset = parent_graph->reverse_edge->at(edge_offset);
-
   //     edges->insert(std::min(edge_offset,reverse_edge_offset));
-
   //     col = parent_graph->rows->at(edge_offset);
   //   }
-
   //   edges->insert(std::min(non_tree_edge_index,parent_graph->reverse_edge->at(non_tree_edge_index)));
-
   //   return edges;
   // }
 
   /**
-   * @brief This method returns a bit_vector corresponding to the edges of the cycle.
-   * @details The cycles are represented using bit_vectors of non_tree edges. Non_tree edges present in 
-   * the cycles are marked set in the bit_vectors at their corresponding positions.
-   * 
+   * @brief
+   * This method returns a bit_vector corresponding to the edges of the cycle.
+   * @details
+   * The cycles are represented using bit_vectors of non_tree edges. Non_tree
+   * edges present in the cycles are marked set in the bit_vectors at their
+   * corresponding positions.
    * @param non_tree_edges map of non_tree edges and its position from 0 - non_tree_edges.size() - 1
    * @return bit_vector describing the cycle.
    */
-  BitVector *get_cycle_vector(std::vector<int> &non_tree_edges,
-      int num_elements) {
+  BitVector *get_cycle_vector(std::vector<int> &non_tree_edges, int num_elements) {
     BitVector *vector = new BitVector(num_elements);
-
     unsigned row = trees->parent_graph->rows->at(non_tree_edge_index);
     unsigned col = trees->parent_graph->cols->at(non_tree_edge_index);
 
@@ -94,7 +80,6 @@ struct cycle {
     unsigned *node_rowoffsets, *node_columns, edge_offset, *nodes_index;
     ;
     int *node_edgeoffsets, *node_parents, *node_distance;
-
     int src_index = trees->get_index(root);
 
     trees->get_node_arrays_warp(&node_rowoffsets, &node_columns,
@@ -104,48 +89,40 @@ struct cycle {
     //check for vertices row =====> root.
     while (node_parents[row] != -1) {
       edge_offset = node_parents[row];
-
       if (non_tree_edges[edge_offset] >= 0)
         vector->set(non_tree_edges[edge_offset], true);
-
       if (trees->parent_graph->rows->at(edge_offset) != row)
         row = trees->parent_graph->rows->at(edge_offset);
       else
         row = trees->parent_graph->cols->at(edge_offset);
-
       assert(row != -1);
     }
 
     //check for vertices col =====> root.
     while (node_parents[col] != -1) {
       edge_offset = node_parents[col];
-
       if (non_tree_edges[edge_offset] >= 0)
         vector->set(non_tree_edges[edge_offset], true);
-
       if (trees->parent_graph->rows->at(edge_offset) != col)
         col = trees->parent_graph->rows->at(edge_offset);
       else
         col = trees->parent_graph->cols->at(edge_offset);
-
       assert(col != -1);
     }
     return vector;
   }
 
   /**
-   * @brief This method returns a bit_vector corresponding to the edges of the cycle.
-   *  @details The cycles are represented using bit_vectors of non_tree edges. Non_tree edges present in
-   * the cycles are marked set in the bit_vectors at their corresponding positions.
-   *
+   * @brief
+   * This method returns a bit_vector corresponding to the edges of the cycle.
+   * @details The cycles are represented using bit_vectors of non_tree edges.
+   * Non_tree edges present in the cycles are marked set in the bit_vectors at
+   * their corresponding positions.
    * @param non_tree_edges map of non_tree edges and its position from 0 - non_tree_edges.size() - 1
    * @return bit_vector describing the cycle.
    */
-  void *get_cycle_vector(std::vector<int> &non_tree_edges, int num_elements,
-      BitVector *cycle_vector) {
-
+  void *get_cycle_vector(std::vector<int> &non_tree_edges, int num_elements, BitVector *cycle_vector) {
     cycle_vector->init();
-
     unsigned row = trees->parent_graph->rows->at(non_tree_edge_index);
     unsigned col = trees->parent_graph->cols->at(non_tree_edge_index);
 
@@ -156,9 +133,7 @@ struct cycle {
     unsigned *node_rowoffsets, *node_columns, edge_offset, *nodes_index;
     ;
     int *node_edgeoffsets, *node_parents, *node_distance;
-
     int src_index = trees->get_index(root);
-
     trees->get_node_arrays_warp(&node_rowoffsets, &node_columns,
         &node_edgeoffsets, &node_parents, &node_distance, &nodes_index,
         src_index);
@@ -166,44 +141,36 @@ struct cycle {
     //check for vertices row =====> root.
     while (node_parents[row] != -1) {
       edge_offset = node_parents[row];
-
       if (non_tree_edges[edge_offset] >= 0)
         cycle_vector->set(non_tree_edges[edge_offset], true);
-
       if (trees->parent_graph->rows->at(edge_offset) != row)
         row = trees->parent_graph->rows->at(edge_offset);
       else
         row = trees->parent_graph->cols->at(edge_offset);
-
       assert(row != -1);
     }
 
     //check for vertices col =====> root.
     while (node_parents[col] != -1) {
       edge_offset = node_parents[col];
-
       if (non_tree_edges[edge_offset] >= 0)
         cycle_vector->set(non_tree_edges[edge_offset], true);
-
       if (trees->parent_graph->rows->at(edge_offset) != col)
         col = trees->parent_graph->rows->at(edge_offset);
       else
         col = trees->parent_graph->cols->at(edge_offset);
-
       assert(col != -1);
     }
   }
 
   void print() {
-    printf(
-        "=================================================================================\n");
+    printf("=================================================================================\n");
     printf("Root is %u\n", root + 1);
     printf("Edge is %u - %u\n",
         trees->parent_graph->rows->at(non_tree_edge_index) + 1,
         trees->parent_graph->cols->at(non_tree_edge_index) + 1);
     printf("Total weight = %d\n", total_length);
-    printf(
-        "=================================================================================\n");
+    printf("=================================================================================\n");
   }
 
   void print_line() {
@@ -211,7 +178,4 @@ struct cycle {
         trees->parent_graph->rows->at(non_tree_edge_index) + 1,
         trees->parent_graph->cols->at(non_tree_edge_index) + 1);
   }
-
 };
-
-#endif
